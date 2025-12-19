500a4cee4a3b8a78fa974c420cf344cbd9499062
estelconchillo <estel.conchillo@gmail.com>
2025-12-19 09:46:48 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
 *Author: Estel Conchillo
 */
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer'; 
 
